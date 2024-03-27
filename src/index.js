f33ec16ea0c188d5456e96f783ae0270b869435e
mashalnaeem <mashalnaeem@hotmail.com>
2024-03-27 12:06:45 -0400

    download bootstrap library for styling through npm

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+// import { Container, Row, Col } from 'react-bootstrap';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
