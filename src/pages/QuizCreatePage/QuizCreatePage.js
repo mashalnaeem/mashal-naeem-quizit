87df26a1fbedb78b3d35c4e2e9b2afe370bf25b6
mashalnaeem <mashalnaeem@hotmail.com>
2024-03-29 01:43:27 -0400

    add create quiz page with input formData that is sent in a post request to add the quiz to the users_quiz table in the database

--- src/pages/QuizCreatePage/QuizCreatePage.js
+++ src/pages/QuizCreatePage/QuizCreatePage.js
@@ -1,28 +1,56 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { useParams } from "react-router-dom"
+import { useParams } from 'react-router-dom';
 
-function CreateUserQuizPage() {
+function QuizCreatePage() {
+    const { userId } = useParams();
 
-const { userId } = useParams();
-  
     const [formData, setFormData] = useState({
         title: '',
         description: '',
         category: '',
-        // difficulty: 'medium', //
-        numQuestions: 0,
-        durationMinutes: 0,
+        difficulty: '',
+        numQuestions: 1,
+        durationMinutes: '',
         isPublic: false,
-        // imageUrl: '',
-        // questions: []
+        questions: [{ question: '', correctAnswer: '', incorrectAnswers: ['', '', ''] }]
     });
 
     const handleChange = (e) => {
-        const { name, value, type, checked } = e.target;
+        const { name, value } = e.target;
         setFormData(prevData => ({
             ...prevData,
-            [name]: type === 'checkbox' ? checked : value
+            [name]: value
+        }));
+    };
+
+    const handleQuestionChange = (e, index, field, subIndex) => {
+        const { name, value } = e.target;
+        const updatedQuestions = [...formData.questions];
+        if (field === 'incorrectAnswers') {
+            updatedQuestions[index].incorrectAnswers[subIndex] = value;
+        } else {
+            updatedQuestions[index][field] = value;
+        }
+        setFormData(prevData => ({
+            ...prevData,
+            questions: updatedQuestions
+        }));
+    };
+    
+
+    const handleRemoveQuestion = (index) => {
+        const updatedQuestions = formData.questions.filter((_, i) => i !== index);
+        setFormData(prevData => ({
+            ...prevData,
+            questions: updatedQuestions
+        }));
+    };
+
+    const handleAddQuestion = () => {
+        setFormData(prevData => ({
+            ...prevData,
+            questions: [...prevData.questions, { question: '', correctAnswer: '', incorrectAnswers: ['', '', ''] }]
         }));
     };
 
@@ -38,6 +66,7 @@ const { userId } = useParams();
         }
     };
 
+console.log(formData)
     return (
         <div>
             <h2>Create User Quiz</h2>
@@ -52,20 +81,35 @@ const { userId } = useParams();
                 </div>
                 <div>
                     <label>Category:</label>
-                    <input type="text" name="category" value={formData.category} onChange={handleChange} required />
+                    <select name="category" value={formData.category} onChange={handleChange} required>
+                        <option value="">Select Category</option>
+                        <option value="Language">Language</option>
+                        <option value="Science">Science</option>
+                        <option value="Math">Math</option>
+                        <option value="History">History</option>
+                        <option value="Social Studies">Social Studies</option>
+                        <option value="Technology">Technology</option>
+                        <option value="Other">Other</option>
+                    </select>
+                    {formData.category === "Other" && (
+                        <input
+                            type="text"
+                            name="otherCategory"
+                            placeholder="Enter Other Category"
+                            value={formData.otherCategory}
+                            onChange={handleChange}
+                            required
+                        />
+                    )}
                 </div>
                 <div>
                     <label>Difficulty:</label>
-                    {/* <select name="difficulty" value={formData.difficulty} onChange={handleChange} required>
+                    <select name="difficulty" value={formData.difficulty} onChange={handleChange} required>
                         <option value="">Select Difficulty</option>
                         <option value="Easy">Easy</option>
-                        <option value="Medium">Medium</option>
+                        <option value="Medium">Intermediate</option>
                         <option value="Hard">Hard</option>
-                    </select> */}
-                </div>
-                <div>
-                    <label>Number of Questions:</label>
-                    <input type="number" name="numQuestions" value={formData.numQuestions} onChange={handleChange} required />
+                    </select>
                 </div>
                 <div>
                     <label>Duration (Minutes):</label>
@@ -75,14 +119,62 @@ const { userId } = useParams();
                     <label>Is Public:</label>
                     <input type="checkbox" name="isPublic" checked={formData.isPublic} onChange={handleChange} />
                 </div>
-                {/* <div>
-                    <label>Image URL:</label>
-                    <input type="text" name="imageUrl" value={formData.imageUrl} onChange={handleChange} />
-                </div> */}
+                <div>
+                    <p>Total Questions: {formData.questions.length}</p>
+                    <button type="button" onClick={handleAddQuestion}>Add Question</button>
+                    {formData.questions.map((question, index) => (
+                        <div key={index}>
+                            <label>Question {index + 1}:</label>
+                            <input
+                                type="text"
+                                name={`questions[${index}].question`}
+                                value={formData.questions[index].question}
+                                onChange={(e) => handleQuestionChange(e, index, 'question')}
+                                required
+                            />
+                            <div>
+                                <label>Correct Answer:</label>
+                                <input
+                                    type="text"
+                                    name={`questions[${index}].correctAnswer`}
+                                    value={formData.questions[index].correctAnswer}
+                                    onChange={(e) => handleQuestionChange(e, index, 'correctAnswer')}
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label>Incorrect Answers:</label>
+                                <input
+                                    type="text"
+                                    name={`questions[${index}].incorrectAnswers[0]`}
+                                    value={formData.questions[index].incorrectAnswers[0]}
+                                    onChange={(e) => handleQuestionChange(e, index, 'incorrectAnswers', 0)}
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    name={`questions[${index}].incorrectAnswers[1]`}
+                                    value={formData.questions[index].incorrectAnswers[1]}
+                                    onChange={(e) => handleQuestionChange(e, index, 'incorrectAnswers', 1)}
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    name={`questions[${index}].incorrectAnswers[2]`}
+                                    value={formData.questions[index].incorrectAnswers[2]}
+                                    onChange={(e) => handleQuestionChange(e, index, 'incorrectAnswers', 2)}
+                                    required
+                                />
+
+                            </div>
+                            <button type="button" onClick={() => handleRemoveQuestion(index)}>Remove</button>
+                        </div>
+                    ))}
+                </div>
                 <button type="submit">Submit</button>
             </form>
         </div>
     );
 }
 
-export default CreateUserQuizPage;
+export default QuizCreatePage;
